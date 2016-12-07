10dedc315034affe81a4e544405d3eb44f248283
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-07 17:05:54 +0000

    adding extraction of name and artists

--- public/app.js
+++ public/app.js
@@ -6,8 +6,37 @@
     var url = "https://api.spotify.com/v1/search?q=christmas&type=album";
     ajaxHelper.makeGetRequest( url, function( responseObject ) {
       albums = responseObject.albums.items;
-      console.log( albums );
+      console.log(albums[0]);
+      populateTable();
     });
+  }
+
+  var populateTable = function() {
+
+    var table = document.querySelector( 'table' );
+    var tHead = document.querySelector( 'thead' );
+    var tBody = document.querySelector( 'tBody' );
+
+    tHead.innerHTML = "";
+    tBody.innerHTML = "";
+
+    var albumsData = [];
+
+    albums.forEach( function( album ) {
+      var albumData = {}
+      albumData.name = album.name;
+
+      var artistNames = album.artists.map( function( artist ) {
+        return artist.name;
+      });
+      albumData.artists = artistNames.reduce( function( output, artistName ) {
+        return output + ", " + artistName;
+      });
+
+      albumsData.push( albumData );
+    });
+
+    console.log( albumsData );
   }
 
   window.onload = app;
