9f84217e4291ade1d2edd5f4d48196d7d6fc2e86
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-07 17:10:44 +0000

    refactoring

--- public/app.js
+++ public/app.js
@@ -1,18 +1,15 @@
 (function() {
 
-  var albums = null;
-
   var app = function(){
     var url = "https://api.spotify.com/v1/search?q=christmas&type=album";
     ajaxHelper.makeGetRequest( url, function( responseObject ) {
-      albums = responseObject.albums.items;
+      var albums = responseObject.albums.items;
       console.log(albums[0]);
-      populateTable();
+      populateTable( albums );
     });
-  }
+  };
 
-  var populateTable = function() {
-
+  var populateTable = function( albums ) {
     var table = document.querySelector( 'table' );
     var tHead = document.querySelector( 'thead' );
     var tBody = document.querySelector( 'tBody' );
@@ -20,6 +17,11 @@
     tHead.innerHTML = "";
     tBody.innerHTML = "";
 
+    var albumsData = extractAlbumData( albums );
+    console.log( albumsData );
+  };
+
+  var extractAlbumData = function( albums ) {
     var albumsData = [];
 
     albums.forEach( function( album ) {
@@ -36,8 +38,8 @@
       albumsData.push( albumData );
     });
 
-    console.log( albumsData );
-  }
+    return albumsData;
+  };
 
   window.onload = app;
 
