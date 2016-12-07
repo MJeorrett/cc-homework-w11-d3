d495c5e8c0705d40f7833c78b48acab1ce0f5072
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-07 16:47:17 +0000

    adding html for table

--- public/app.js
+++ public/app.js
@@ -1,9 +1,12 @@
 (function() {
 
+  var albums = null;
+
   var app = function(){
     var url = "https://api.spotify.com/v1/search?q=christmas&type=album";
     ajaxHelper.makeGetRequest( url, function( responseObject ) {
-      console.log( responseObject );
+      albums = responseObject.albums.items;
+      console.log( albums );
     });
   }
 
