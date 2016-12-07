76ff543b480c1352eca0876cef0b18b71a0f01b5
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-07 18:03:24 +0000

    adding hidden id column

--- public/app.js
+++ public/app.js
@@ -17,6 +17,11 @@
     tHead.innerHTML = "";
 
     var columnHeadings = [ "Name", "Artists", "Images" ];
+
+    var idTd = htmlHelper.create( 'td', "_id" );
+    idTd.style.display = "none";
+    tHead.appendChild( idTd );
+
     columnHeadings.forEach( function( columnHeading ) {
       var td = htmlHelper.create( 'td', columnHeading );
       tHead.appendChild( td )
@@ -27,14 +32,19 @@
 
     albumsData.forEach( function( albumData ) {
       var tr = htmlHelper.create( 'tr' );
+      var idTd = htmlHelper.create( 'td', albumData.id );
+      idTd.style.display = "none";
       var nameTd = htmlHelper.create( 'td', albumData.name );
       var artistsTd = htmlHelper.create( 'td', albumData.artists );
       var imageTd = htmlHelper.create( 'td' );
       var image = htmlHelper.create( 'img' );
       image.src = albumData.imageUrls[0];
       image.classList.add( 'album-image' );
+      image.onclick = handleImageClicked;
+
       imageTd.appendChild( image );
 
+      tr.appendChild( idTd );
       tr.appendChild( nameTd );
       tr.appendChild( artistsTd );
       tr.appendChild( imageTd );
@@ -42,11 +52,18 @@
     });
   };
 
+  var handleImageClicked = function( ev ) {
+    var row = ev.target.parentNode.parentNode;
+    var albumId = row.firstChild.innerText;
+    console.log( "clicked album id:", albumId );
+  };
+
   var extractAlbumData = function( albums ) {
     var albumsData = [];
 
     albums.forEach( function( album ) {
       var albumData = {}
+      albumData.id = album.id;
       albumData.name = album.name;
 
       var artistNames = album.artists.map( function( artist ) {
