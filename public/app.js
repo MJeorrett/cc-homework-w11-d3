8dba61f2ea52f4d702a35de003f42ebe669e7295
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-07 16:40:55 +0000

    adding whitespace

--- public/app.js
+++ public/app.js
@@ -1,9 +1,12 @@
 (function() {
+
   var app = function(){
     var url = "https://api.spotify.com/v1/search?q=christmas&type=album";
     ajaxHelper.makeGetRequest( url, function( responseObject ) {
       console.log( responseObject );
     });
   }
+
   window.onload = app;
+
 })();
